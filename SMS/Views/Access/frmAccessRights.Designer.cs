c03516b7d7f7a8c7e02f57b40ce1bce05ca257f0
dbyamyamin <dbyamyamin@gmail.com>
2022-10-11 11:35:50 +0800

    add checkbox 10112022

--- SMS/Views/Access/frmAccessRights.Designer.cs
+++ SMS/Views/Access/frmAccessRights.Designer.cs
@@ -30,24 +30,24 @@ namespace SMS.Views.Access
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.cboposition = new System.Windows.Forms.ComboBox();
-            this.dgvSystem = new System.Windows.Forms.DataGridView();
-            this.label2 = new System.Windows.Forms.Label();
-            this.cbosystem = new System.Windows.Forms.ComboBox();
-            this.cbomodule = new System.Windows.Forms.ComboBox();
-            this.dgvModule = new System.Windows.Forms.DataGridView();
-            this.label3 = new System.Windows.Forms.Label();
+            this.btnmodule = new System.Windows.Forms.Button();
+            this.btnsystem = new System.Windows.Forms.Button();
             this.btnsave = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgvModule = new System.Windows.Forms.DataGridView();
+            this.cbomodule = new System.Windows.Forms.ComboBox();
+            this.cbosystem = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvSystem = new System.Windows.Forms.DataGridView();
+            this.cboposition = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.btnsystem = new System.Windows.Forms.Button();
-            this.btnmodule = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvSystem)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvModule)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSystem)).BeginInit();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -69,73 +69,37 @@ namespace SMS.Views.Access
             this.panel1.Size = new System.Drawing.Size(324, 341);
             this.panel1.TabIndex = 0;
             // 
-            // label1
+            // btnmodule
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 16);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Position Name";
+            this.btnmodule.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnmodule.Location = new System.Drawing.Point(249, 183);
+            this.btnmodule.Name = "btnmodule";
+            this.btnmodule.Size = new System.Drawing.Size(51, 24);
+            this.btnmodule.TabIndex = 10;
+            this.btnmodule.Text = "+ Add";
+            this.btnmodule.UseVisualStyleBackColor = true;
+            this.btnmodule.Click += new System.EventHandler(this.btnmodule_Click);
             // 
-            // cboposition
+            // btnsystem
             // 
-            this.cboposition.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cboposition.FormattingEnabled = true;
-            this.cboposition.Location = new System.Drawing.Point(104, 9);
-            this.cboposition.Name = "cboposition";
-            this.cboposition.Size = new System.Drawing.Size(135, 23);
-            this.cboposition.TabIndex = 1;
+            this.btnsystem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsystem.Location = new System.Drawing.Point(245, 43);
+            this.btnsystem.Name = "btnsystem";
+            this.btnsystem.Size = new System.Drawing.Size(55, 24);
+            this.btnsystem.TabIndex = 10;
+            this.btnsystem.Text = "+Add";
+            this.btnsystem.UseVisualStyleBackColor = true;
+            this.btnsystem.Click += new System.EventHandler(this.btnsystem_Click);
             // 
-            // dgvSystem
+            // btnsave
             // 
-            this.dgvSystem.AllowUserToAddRows = false;
-            this.dgvSystem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvSystem.Location = new System.Drawing.Point(19, 76);
-            this.dgvSystem.Name = "dgvSystem";
-            this.dgvSystem.RowHeadersVisible = false;
-            this.dgvSystem.Size = new System.Drawing.Size(282, 95);
-            this.dgvSystem.TabIndex = 2;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(14, 47);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(84, 15);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "System Name";
-            // 
-            // cbosystem
-            // 
-            this.cbosystem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cbosystem.FormattingEnabled = true;
-            this.cbosystem.Location = new System.Drawing.Point(104, 43);
-            this.cbosystem.Name = "cbosystem";
-            this.cbosystem.Size = new System.Drawing.Size(135, 23);
-            this.cbosystem.TabIndex = 4;
-            // 
-            // cbomodule
-            // 
-            this.cbomodule.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cbomodule.FormattingEnabled = true;
-            this.cbomodule.Location = new System.Drawing.Point(111, 183);
-            this.cbomodule.Name = "cbomodule";
-            this.cbomodule.Size = new System.Drawing.Size(128, 23);
-            this.cbomodule.TabIndex = 6;
-            this.cbomodule.SelectedIndexChanged += new System.EventHandler(this.cbomodule_SelectedIndexChanged);
-            // 
-            // dgvModule
-            // 
-            this.dgvModule.AllowUserToAddRows = false;
-            this.dgvModule.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvModule.Location = new System.Drawing.Point(19, 213);
-            this.dgvModule.Name = "dgvModule";
-            this.dgvModule.RowHeadersVisible = false;
-            this.dgvModule.Size = new System.Drawing.Size(282, 95);
-            this.dgvModule.TabIndex = 7;
+            this.btnsave.Location = new System.Drawing.Point(217, 316);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(67, 23);
+            this.btnsave.TabIndex = 9;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
             // 
             // label3
             // 
@@ -147,15 +111,75 @@ namespace SMS.Views.Access
             this.label3.TabIndex = 8;
             this.label3.Text = "Module Name";
             // 
-            // btnsave
+            // dgvModule
             // 
-            this.btnsave.Location = new System.Drawing.Point(217, 316);
-            this.btnsave.Name = "btnsave";
-            this.btnsave.Size = new System.Drawing.Size(67, 23);
-            this.btnsave.TabIndex = 9;
-            this.btnsave.Text = "Save";
-            this.btnsave.UseVisualStyleBackColor = true;
-            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            this.dgvModule.AllowUserToAddRows = false;
+            this.dgvModule.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvModule.Location = new System.Drawing.Point(19, 213);
+            this.dgvModule.Name = "dgvModule";
+            this.dgvModule.RowHeadersVisible = false;
+            this.dgvModule.Size = new System.Drawing.Size(282, 95);
+            this.dgvModule.TabIndex = 7;
+            this.dgvModule.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvModule_CellClick);
+            // 
+            // cbomodule
+            // 
+            this.cbomodule.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbomodule.FormattingEnabled = true;
+            this.cbomodule.Location = new System.Drawing.Point(111, 183);
+            this.cbomodule.Name = "cbomodule";
+            this.cbomodule.Size = new System.Drawing.Size(128, 23);
+            this.cbomodule.TabIndex = 6;
+            this.cbomodule.SelectedIndexChanged += new System.EventHandler(this.cbomodule_SelectedIndexChanged);
+            // 
+            // cbosystem
+            // 
+            this.cbosystem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbosystem.FormattingEnabled = true;
+            this.cbosystem.Location = new System.Drawing.Point(104, 43);
+            this.cbosystem.Name = "cbosystem";
+            this.cbosystem.Size = new System.Drawing.Size(135, 23);
+            this.cbosystem.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "System Name";
+            // 
+            // dgvSystem
+            // 
+            this.dgvSystem.AllowUserToAddRows = false;
+            this.dgvSystem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSystem.Location = new System.Drawing.Point(19, 76);
+            this.dgvSystem.Name = "dgvSystem";
+            this.dgvSystem.RowHeadersVisible = false;
+            this.dgvSystem.Size = new System.Drawing.Size(282, 95);
+            this.dgvSystem.TabIndex = 2;
+            this.dgvSystem.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvSystem_CellClick);
+            // 
+            // cboposition
+            // 
+            this.cboposition.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboposition.FormattingEnabled = true;
+            this.cboposition.Location = new System.Drawing.Point(104, 9);
+            this.cboposition.Name = "cboposition";
+            this.cboposition.Size = new System.Drawing.Size(135, 23);
+            this.cboposition.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Position Name";
             // 
             // toolStrip1
             // 
@@ -175,17 +199,6 @@ namespace SMS.Views.Access
             this.panel2.Size = new System.Drawing.Size(111, 338);
             this.panel2.TabIndex = 2;
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Elephant", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(-2, 135);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(101, 31);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Access";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
@@ -196,27 +209,16 @@ namespace SMS.Views.Access
             this.label5.TabIndex = 1;
             this.label5.Text = "Rights";
             // 
-            // btnsystem
+            // label4
             // 
-            this.btnsystem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnsystem.Location = new System.Drawing.Point(245, 43);
-            this.btnsystem.Name = "btnsystem";
-            this.btnsystem.Size = new System.Drawing.Size(55, 24);
-            this.btnsystem.TabIndex = 10;
-            this.btnsystem.Text = "+Add";
-            this.btnsystem.UseVisualStyleBackColor = true;
-            this.btnsystem.Click += new System.EventHandler(this.btnsystem_Click);
-            // 
-            // btnmodule
-            // 
-            this.btnmodule.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnmodule.Location = new System.Drawing.Point(249, 183);
-            this.btnmodule.Name = "btnmodule";
-            this.btnmodule.Size = new System.Drawing.Size(51, 24);
-            this.btnmodule.TabIndex = 10;
-            this.btnmodule.Text = "+ Add";
-            this.btnmodule.UseVisualStyleBackColor = true;
-            this.btnmodule.Click += new System.EventHandler(this.btnmodule_Click);
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Elephant", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(-2, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 31);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Access";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // frmAccessRights
             // 
@@ -231,8 +233,8 @@ namespace SMS.Views.Access
             this.Load += new System.EventHandler(this.frmAccessRights_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvSystem)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvModule)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSystem)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.ResumeLayout(false);
